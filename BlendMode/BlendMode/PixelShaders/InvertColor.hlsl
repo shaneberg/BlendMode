3cca72090c904668fabc93474f0928a192e66ee2
Sean Haneberg <sean_haneberg@hotmail.com>
2015-08-24 21:26:15 -0700

    Restructure directories and delete existing app

--- BlendMode/BlendMode/PixelShaders/InvertColor.hlsl
+++
@@ -1,7 +0,0 @@
-sampler2D InputTexture;
-
-float4 main(float2 uv : TEXCOORD) : COLOR{
-    float4 color = tex2D(InputTexture, uv);
-    float4 invertedColor = float4(color.a - color.rgb, color.a);
-    return invertedColor;
-}
