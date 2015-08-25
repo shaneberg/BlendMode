e01789b2d71e5083b3b9cefb1b439b6375ba2167
Sean Haneberg <sean_haneberg@hotmail.com>
2015-08-24 21:13:36 -0700

    Fix build script. Integrate in to VS Build.

--- BlendMode/BlendMode/BuildScripts/BuildShaders.cmd
+++ BlendMode/BlendMode/BuildScripts/BuildShaders.cmd
@@ -3,7 +3,13 @@
 SET FXCCMD="C:\Program Files (x86)\Microsoft DirectX SDK (June 2010)\Utilities\Bin\x64\FXC.EXE"
 SET RELATIVE_PS_HLSL=..\PixelShaders\
 SET RELATIVE_PS_OUT=..\CompiledShaders\
+SET FILEPATH=%~dp0
 
 PUSHD %~dp0%RELATIVE_PS_HLSL%
 
-FOR /R %%G in (*.HLSL) DO %FXCCMD% %%G /E main /T:ps_2_0 /Fo %RELATIVE_PS_OUT%%%G.ps
+MKDIR -P %RELATIVE_PS_OUT%
+REM ERASE %RELATIVE_PS_OUT%*.PS
+
+FOR /R %%G in (*.HLSL) DO %FXCCMD% %%G /E main /T:ps_2_0 /Cc /Ni /Fo %RELATIVE_PS_OUT%\%%~nG.ps
+
+POPD
