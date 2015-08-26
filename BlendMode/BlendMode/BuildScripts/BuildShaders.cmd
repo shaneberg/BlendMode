c3e76a22ba415d84b53453a0816f94b2d570021f
Sean Haneberg <sean_haneberg@hotmail.com>
2015-08-25 22:27:49 -0700

    Shaders compilation added to the build.

---
+++ BlendMode/BlendMode/BuildScripts/BuildShaders.cmd
@@ -0,0 +1,15 @@
+@ECHO OFF
+
+SET FXCCMD="C:\Program Files (x86)\Microsoft DirectX SDK (June 2010)\Utilities\Bin\x64\FXC.EXE"
+SET RELATIVE_PS_HLSL=..\PixelShaders\
+SET RELATIVE_PS_OUT=..\CompiledShaders\
+SET FILEPATH=%~dp0
+
+PUSHD %~dp0%RELATIVE_PS_HLSL%
+
+MKDIR -P %RELATIVE_PS_OUT%
+REM ERASE %RELATIVE_PS_OUT%*.PS
+
+FOR /R %%G in (*.HLSL) DO %FXCCMD% %%G /E main /T:ps_2_0 /Cc /Ni /Fo %RELATIVE_PS_OUT%\%%~nG.ps
+
+POPD
---
+++ BlendMode/BlendMode/PixelShaders/InvertColor.hlsl
@@ -0,0 +1,7 @@
+sampler2D InputTexture;
+
+float4 main(float2 uv : TEXCOORD) : COLOR{
+    float4 color = tex2D(InputTexture, uv);
+    float4 invertedColor = float4(color.a - color.rgb, color.a);
+    return invertedColor;
+}
